7872388f29e32f9102a5bf662794e7a016300cb3
runze26 <n>
2019-10-08 13:10:39 +0800

    Updates

---
+++ precache-manifest.1962d487c31a6f19c7aabf44dd8e58fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47c4cb596b5b9319cb8ffae79f018c4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9bc737d4417498657233",
+    "url": "/monsters-rolodex/static/css/main.d27f9297.chunk.css"
+  },
+  {
+    "revision": "6d818e14018bf00f87cc",
+    "url": "/monsters-rolodex/static/js/2.75c8a6e8.chunk.js"
+  },
+  {
+    "revision": "9bc737d4417498657233",
+    "url": "/monsters-rolodex/static/js/main.64296475.chunk.js"
+  },
+  {
+    "revision": "0d0f416e7b5fe29edb74",
+    "url": "/monsters-rolodex/static/js/runtime-main.9ce5b26a.js"
+  }
+]);
